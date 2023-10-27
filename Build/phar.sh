1e555f8925ac8fb082db66dfa226cb438ae37dad
Rafael Kähm <rafael.kaehm@dkd.de>
2023-10-27 12:38:57 +0200

    [TASK] Refactor CI

--- Build/phar.sh
+++ Build/phar.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-SCRIPT_PATH=$( cd $(dirname "${BASH_SOURCE[0]}"); pwd -P )
-ROOT_PATH="$SCRIPT_PATH/../"
+PHAR_BUILD_PATH="/tmp/php-solr-explain"
+PHAR_COMPOSER_BINARY="/tmp/phar-composer"
 
-cd "$ROOT_PATH"
-rm -fR .Build
-composer install --no-dev
+if [[ ! -f $PHAR_COMPOSER_BINARY ]]; then
+  wget https://github.com/clue/phar-composer/releases/download/v1.4.0/phar-composer-1.4.0.phar -O $PHAR_COMPOSER_BINARY
+  chmod +x $PHAR_COMPOSER_BINARY
+fi
 
-cd ..
-wget https://github.com/clue/phar-composer/releases/download/v1.4.0/phar-composer-1.4.0.phar -O phar-composer
-php phar-composer build "$ROOT_PATH/../php-solr-explain"
+rm -Rf $PHAR_BUILD_PATH
+mkdir $PHAR_BUILD_PATH
+git archive --format=tar --prefix=php-solr-explain/ HEAD | (cd "/tmp" && tar xf -)
+composer install --no-dev --working-dir=$PHAR_BUILD_PATH
+
+mkdir -p ".Build/bin"
+/tmp/phar-composer build $PHAR_BUILD_PATH ".Build/bin/php-solr-explain.phar"
+
+rm -Rf $PHAR_COMPOSER_BINARY $PHAR_BUILD_PATH
